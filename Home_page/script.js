d306a9e6ef22219d356e1ce2cd10ccc9e673fb4d
codeudi <uditaggarwal111@gmail.com>
2023-08-05 00:53:13 +0530

    basic functionality ready

--- Home_page/script.js
+++ Home_page/script.js
@@ -8,12 +8,19 @@ var1.addEventListener("click",()=>{
 let var3=document.getElementById("addblog")
 let var4=document.getElementsByClassName("blogs")
 
-
+let var5=document.querySelectorAll(".blog_modal *")
+var5[0].addEventListener("click",()=>{
+    var2[0].classList.add("disp")
+})
 
 var3.addEventListener("click",()=>{
+
+    let imgurl=var5[1].value
+    let title=var5[2].value
+    let descrip=var5[3].value
     var2[0].classList.add("disp")
     var4[0].innerHTML=var4[0].innerHTML+
-    '<div class="box"> <img src= ${img.src} class="picture"/> <p class="text" id="blog_title">Hello this is the blog title and demo blog \</p> <p id="blog_desc" class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p><button id="read">Read</button></div>'
+    '<div class="box"> <img src="'+imgurl+'"class="picture"/> <p class="text" id="blog_title">'+title+'</p> <p id="blog_desc" class="text">'+descrip+'</p><button id="read">Read</button></div>'
 })
 
 
